e32fae2906da8db42df104487a044ac84372aa0f
Tony Denizard <63482462+Denizard1@users.noreply.github.com>
2022-09-19 20:03:34 -0400

    Starter Cloud System

---
+++ sample.php
@@ -0,0 +1,9 @@
+<table border=1>
+<tr>
+<td> header 1</td>
+<td> header 2</td>
+</tr>
+<tr>
+<td> item 1</td>
+<td> item 2</td>
+</tr>
---
+++ startercloudsystem/includes/connect.php
@@ -0,0 +1,24 @@
+<?php
+//$path = '/includes';
+//set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+
+$server = "localhost";
+$dbusername = "starter";
+$password = "nrA]bGqSaMOpGh/v";
+$db = "starter";
+$debug = "true";
+
+$dbconn = mysqli_connect($server, $dbusername, $password, $db);
+
+if ($dbconn->connect_error) {
+    die('Could not connect: ' . $dbconn->connect_error);
+}elseif($debug == "true"){
+	echo nl2br("\nDEBUG:\n");
+	echo nl2br("3 \n 2 \n 1...");
+	echo nl2br("\n Connected successfully\n");
+}
+
+//You can use the command below to set the default database to another db.
+//mysqli_select_db($dbconn, "webiii");
+
+?>
