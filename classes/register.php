9388acf2df755cd13ade097a77536979771584ef
charlieolinsky <charlesolinsky@gmail.com>
2022-10-12 11:18:33 -0400

    attempt to access classes
    ignore this

--- classes/register.php
+++ classes/register.php
@@ -1,24 +1,34 @@
 <?php 
 
     include_once("../includes/connect.php"); 
-    include_once("../classes/User.php"); 
-
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     
-        $username = $_POST['uname'];
-        $email = $_POST['email'];
-        $password = $_POST['pword'];
 
-        $sql = "INSERT INTO users (username, email, hashPassword) VALUES ('$username', '$email', '$password')";
+    $username = $_POST['uname'];
+    $email = $_POST['email'];
+    $password = $_POST['pword'];
 
-        if ($dbconn->query($sql) === TRUE) {
-            header("location: ../forms/login.html");
-        } 
-        else {
-            echo "Error: " . $sql . "<br>" . $dbconn->error;
-        }
+
+    require_once("../classes/User.php"); 
+    $testUser = new User();
+    $testUser->createUser($username, $email, $password); 
+
+
+    // if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    
+    //     $username = $_POST['uname'];
+    //     $email = $_POST['email'];
+    //     $password = $_POST['pword'];
+
+    //     $sql = "INSERT INTO users (username, email, hashPassword) VALUES ('$username', '$email', '$password')";
+
+    //     if ($dbconn->query($sql) === TRUE) {
+    //         header("location: ../forms/login.html");
+    //     } 
+    //     else {
+    //         echo "Error: " . $sql . "<br>" . $dbconn->error;
+    //     }
  
-    }
+    // }
     
 
 ?>
