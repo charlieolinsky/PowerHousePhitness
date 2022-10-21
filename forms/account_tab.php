e2190cf880d947ecf145ba680fff3561f204b21e
Christina Rodriguez <christina.e.rod@gmail.com>
2022-10-21 13:57:40 -0400

    Added html for "account" tab

---
+++ forms/account_tab.php
@@ -0,0 +1,84 @@
+<!-- code for where a member will check out equipment -->
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Account Info</title>
+
+<link rel="stylesheet" href="../UI/css/bootstrap.min.css">
+<link rel="stylesheet" href="../UI/css/font-awesome.min.css">
+<link rel="stylesheet" href="../UI/css/aos.css">
+<link rel="stylesheet" href="../UI/css/tooplate-gymso-style.css">
+
+
+
+
+</head>
+
+<body data-spy="scroll" data-target="#navbarNav" data-offset="50">
+<!-- MENU BAR -->
+<nav class="navbar navbar-expand-lg fixed-top">
+<div class="container">
+
+<a class="navbar-brand" href="index.html">Power House Phitness</a>
+
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse" id="navbarNav">
+<ul class="navbar-nav ml-lg-auto">
+<li class="nav-item">
+<a href="#home" class="nav-link smoothScroll">Home</a>
+</li>
+
+<li class="nav-item">
+<a href="about.html" class="nav-link">About Us</a>
+</li>
+
+<li class="nav-item">
+<a href="#class" class="nav-link smoothScroll">Classes</a>
+</li>
+
+<li class="nav-item">
+<a href="#schedule" class="nav-link smoothScroll">Schedules</a>
+</li>
+
+<li class="nav-item">
+<a href="#contact" class="nav-link smoothScroll">Contact</a>
+</li>
+</ul>
+
+<ul class="social-icon ml-lg-3">
+<li><a href="https://fb.com/tooplate" class="fa fa-facebook"></a></li>
+<li><a href="#" class="fa fa-twitter"></a></li>
+<li><a href="#" class="fa fa-instagram"></a></li>
+</ul>
+</div>
+
+</div>
+</nav>
+<br><br><br>
+
+<main> 
+<h1>My Account</h1>
+<p>Need to rent equipment while visiting our facility? We have a variety of options from basketballs to shoot hoops, to frisbees to toss around our open gym!
+</p>
+
+<div class = "account-info">
+    <dl>
+        <dt>User ID: </dt>
+        <dt>Name: </dd>
+        <dt>Email: </dt>
+        <dt>Address: </dt>
+        <dt>Phone Number: </dt>
+        <dt>Member Ship Level: </dt>
+        <dt>Locker Number: </dt>
+      </dl>
+</div>
+</main>
+</body>
+</html>
