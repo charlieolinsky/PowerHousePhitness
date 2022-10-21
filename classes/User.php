ba3e61a5bd430a0d65de4ea77b623d64dedce7c1
Christina Rodriguez <christina.e.rod@gmail.com>
2022-10-21 13:58:04 -0400

    Modified user class and login form

--- classes/User.php
+++ classes/User.php
@@ -1,39 +1,96 @@
 <?php
 
+// THIS INCLUDES ERROR HANDLING- password requirements, verify match, checks if a field was left empty 
+
 class User {
 //properties
-private Name $name;
 private $id;
-private Address $address; // mailing address 
+private $firstName; 
+private $lastName;
+private $address; // mailing/billing address ?
 private $email;
 private $phoneNumber;
 private $password; //username will be email
-//constructor
-function __User($n,$id,$add,$em,$pn,$pass){
-    $this->name = $n;
-    $this->id = $id;
-    $this->address = $add;
+private $lockerNumber; // default null -------------> table for locker # and combo pairs?
+private $lockerCombo; // deafult null
+private $membershipLevel; // default 1 (free member)
+
+//methods
+public function _User($n, $ln, $em, $pass)
+{
+    $this->firstName = $n;
+    $this->lastName = $ln;
     $this->email = $em;
-    $this->phoneNumber = $pn;
     $this->password = $pass;
 }
-//methods
-function createUser()
-{
-    // ? constructor?
+function createUser(){
+
+    if (empty($_POST["fname"])) {
+        die("Name is required");
+    }
+    if (empty($_POST["lname"])) {
+        die("Name is required");
+    }
+    if ( ! filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+        die("Valid email is required");
+    }
+    
+    if (strlen($_POST["pword"]) < 8) {
+        die("Password must be at least 8 characters");
+    }
+    
+    if ( ! preg_match("/[a-z]/i", $_POST["pword"])) {
+        die("Password must contain at least one letter");
+    }
+    
+    if ( ! preg_match("/[0-9]/", $_POST["pword"])) {
+        die("Password must contain at least one number");
+    }
+    
+    if ($_POST["pword"] !== $_POST["vword"]) {
+        die("Passwords must match");
+    }
+    
+    $password_hash = password_hash($_POST["pword"], PASSWORD_DEFAULT);
+    
+    $mysqli = require __DIR__ . "/connect.php";
+    
+    $sql = "INSERT INTO basic_users (id, firstName, lastName, address, email, phoneNumber, password, lockerNumber, lockerCombo, membershipLevel)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            
+    $stmt = $mysqli->stmt_init();
+    
+    if ( ! $stmt->prepare($sql)) {
+        die("SQL error: " . $mysqli->error);
+    }
+    
+    $stmt->bind_param("sss",
+                      $_POST["name"],
+                      $_POST["email"],
+                      $password_hash);
+                      
+    if ($stmt->execute()) {
+    
+        header("Location: signup-success.html");
+        exit;
+        
+    } else {
+        
+        if ($mysqli->errno === 1062) {
+            die("email already taken");
+        } else {
+            die($mysqli->error . " " . $mysqli->errno);
+        }
+    }
 }
-function editUser()
+// setters/getters
+function getFirstName()
 {
-    // ? setters?
+    return $this->firstName;
 }
-function removeUser()
+function getLastName()
 {
-    // ? remove from DB
-}
-//getters and setters
-function getName()
-{
-    return $this->name;
+    return $this->lastName;
 }
 function getId()
 {
@@ -55,9 +112,25 @@ function getPassword()
 {   return $this->password;
 
 }
-function setName($n)
+function getLockerNumber()
+{   return $this->lockerNumber;
+
+}
+function getLockerCombo()
+{   return $this->lockerCombo;
+
+}
+function getMembershipLevel()
+{   return $this->membershipLevel;
+
+}
+function setFirstName($n)
 {
-    $this->name = $n;
+    $this->firstName = $n;
+}
+function setLastName($n)
+{
+    $this->lastName = $n;
 }
 function setId($id)
 {
@@ -79,10 +152,21 @@ function setPassword($pass)
 {   $this->password = $pass;
 
 }
-function userDetails()
+function setLockerNumber($pass)
+{   $this->password = $pass;
+
+}
+function setLockerCombo($pass)
+{   $this->password = $pass;
+
+}
+function setMembershipLevel($pass)
+{   $this->password = $pass;
+
+}
+function userDetails() // make this echo?
 {
     return "Name: $this->name\n ID: $this->id\n Address: $this->address\n Email: $this->email\n Phone Number: $this->phoneNumber\n";
 }
 }
-
 ?>
