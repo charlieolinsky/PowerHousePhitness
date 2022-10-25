1077fd74f78d15ba6d8bfbc129d7b89b50dc9606
olakuma <paul_kumapayi@yahoo.com>
2022-10-25 01:06:57 -0400

    update

--- include/config.php
+++ include/config.php
@@ -1,9 +1,9 @@
 <?php 
-// require_once("../sql/connect.php"); 
+include_once("../sql/connect.php"); 
 // Common settings 
 define('CURRENCY', 'USD'); 
 define('CURRENCY_SYMBOL', '$'); 
   
-
+$dbconn ->close();
 
 ?>
