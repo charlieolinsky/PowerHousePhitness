5ef447f0788ea50d6f76b5be24bc721df9fe3ffa
charlieolinsky <charlesolinsky@gmail.com>
2022-10-25 11:31:21 -0400

    Initialized Sessions Class

--- classes/session.php
+++ classes/session.php
@@ -1,1 +1,24 @@
-<!-- Will Contain Code for Sessions Class  -->
+<?php 
+
+    class Session {
+
+        private $session_data; 
+
+        public function _Session(){
+
+            $this->session_data = []; 
+
+        }
+
+        public function startSession(){
+
+        }
+
+        public function endSesssion(){
+
+        }
+
+
+    }
+
+?>
