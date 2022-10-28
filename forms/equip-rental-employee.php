7bb80400724e1a543a00b798f5524687201e909a
beesanne <beesanne.kurzum@gmail.com>
2022-10-27 20:58:28 -0400

    tested connecting to a shopping cart

--- forms/equip-rental-employee.php
+++ forms/equip-rental-employee.php
@@ -1,15 +1,17 @@
 <!-- code that will have waht appears when general employee checks something back in -->
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Employee Portal - Rentals</title>
 
-
-
 </head>
+
 <body>
-    
+
+   
 </body>
+
 </html>
