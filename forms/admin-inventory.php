ebf33c55d38d03a955da12b1fbe5a012b9bac3b3
beesanne <beesanne.kurzum@gmail.com>
2022-10-27 20:59:09 -0400

    added name field to add inventory item

--- forms/admin-inventory.php
+++ forms/admin-inventory.php
@@ -1,13 +1,38 @@
 <!-- code to hold the what will appear on the admin-inventory page  -->
 
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Admin Inventory</title>
 </head>
+
 <body>
-    
+
+    <div class="add_inventory">
+        <h1> this is the admin inventory page </h1>
+
+        <?php
+        include_once("../include/add-products.php");
+        ?>
+
+
+        <form action="../include/add-products.php" method="post">
+
+            <label for="prod_name"> Item Name</label><br>
+            <input name="prod_name" id="prod_name" type="text"><br><br>
+
+            <br>
+            <input type="submit" name="submit" value="Submit Form"><br>
+
+        </form>
+
+    </div>
+
+
+
 </body>
+
 </html>
