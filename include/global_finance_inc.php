3959a3fd2688b0361db2e30d800afbc614ec3e3c
charlieolinsky <charlesolinsky@gmail.com>
2022-10-31 19:52:31 -0400

    global include files
    -clarification required

---
+++ include/global_finance_inc.php
@@ -0,0 +1,6 @@
+<?php
+    include_once("../classes/FinancialReports.php");
+    include_once("../classes/ShoppingCartClass.php");
+    include_once("../classes/Roles.php");
+
+?>
---
+++ include/global_var_inc.php
@@ -0,0 +1,10 @@
+<?php 
+
+    //varibles for the user 
+    //for cart
+    //for roles
+    //for status (checking out, renting, )
+    //for timestamps
+    // 
+
+?>
