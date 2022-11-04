e4e48edefefd62aaa3b42c61c990bcebfbe40f6f
beesanne <beesanne.kurzum@gmail.com>
2022-11-04 00:49:43 -0400

    added vendor drop down menu to add to DB (works!)

--- forms/admin-inventory.php
+++ forms/admin-inventory.php
@@ -24,8 +24,6 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     } else if (is_int($_POST['prod_quantity'])) {
         $quantity_error = "Invalid input, please enter a number.";
     }
-    //Vendor ID Validation 
-
 
     //Date Purchased Validation
     if (empty($_POST['prod_date_purchased'])) {
@@ -67,15 +65,15 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
             <h1> this is the admin inventory page </h1>
 
-
-            <!-- <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST"> -->
-            <form action="../include/add-products.php" method="POST">
+            <!-- this line lets the data validation work but does not add to DB -->
+            <!-- <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">  -->
+            <!-- this line doesnt do datavalidation but DOES insert into b -->
+                <form action="../include/add-products.php" method="POST">
 
 
                 <!-- the name="__" field is what connects this form to the querying file -->
                 <label for="prod_name"> Item Name:</label><br>
-                <input type="text" id="prod_name" name="prod_name" 
-                       value="<?php if (isset($_POST['prod_name'])) echo $_POST['prod_name'];?>">
+                <input type="text" id="prod_name" name="prod_name" value="<?php if (isset($_POST['prod_name'])) echo $_POST['prod_name']; ?>">
                 <br>
                 <span> <?php if (isset($name_error)) echo $name_error; ?> </span>
                 <br><br>
@@ -83,8 +81,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
                 <!-- asking for prod description -->
                 <label for="prod_desc"> Item Description:</label><br>
-                <input type="text" id="prod_desc" name="prod_desc"
-                value="<?php if (isset($_POST['prod_desc'])) echo $_POST['prod_desc'];?>">
+                <input type="text" id="prod_desc" name="prod_desc" value="<?php if (isset($_POST['prod_desc'])) echo $_POST['prod_desc']; ?>">
                 <br>
                 <span> <?php if (isset($desc_error)) echo $desc_error; ?> </span>
                 <br><br>
@@ -96,40 +93,51 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
                 <!-- need to add validation to be a # -->
                 <label for="prod_price"> Item Price: </label><br>
-                <input type="text" id="prod_price" name="prod_price"                 
-                value="<?php if (isset($_POST['prod_price'])) echo $_POST['prod_price'];?>">
+                <input type="text" id="prod_price" name="prod_price" value="<?php if (isset($_POST['prod_price'])) echo $_POST['prod_price']; ?>">
                 <br>
                 <span> <?php if (isset($price_error)) echo $price_error; ?> </span>
                 <br><br>
 
                 <!-- need to add validation to be a # -->
                 <label for="prod_quantity">Item Quantity: </label><br>
-                <input type="text" id="prod_quantity" name="prod_quantity"
-                value="<?php if (isset($_POST['prod_quantity'])) echo $_POST['prod_quantity'];?>">
+                <input type="text" id="prod_quantity" name="prod_quantity" value="<?php if (isset($_POST['prod_quantity'])) echo $_POST['prod_quantity']; ?>">
                 <!-- value is making the form hold its value after submit if there wa an error -->
                 <br>
                 <span> <?php if (isset($quantity_error)) echo $quantity_error; ?> </span>
                 <br><br>
 
-                <!-- Need to add drop down to be connected to the Vendor ID Table  -->
+                <!-- Asking for Vendor ID -->
                 <label for="VENDOR_ID">Vendor: </label><br>
-                <input type="text" id="VENDOR_ID" name="VENDOR_ID" 
-                value="<?php if (isset($_POST['VENDOR_ID'])) echo $_POST['VENDOR_ID'];?>">
-                <br><br>
+                
+                <?php
 
+                include_once("../sql/connect.php");
+
+                // query to get only the vendor ID for the dropdown menu
+                $vendorquery = "SELECT * FROM vendor_id";
+                if ($r_set = $dbconn->query($vendorquery)) {
+                    echo "<SELECT name='VENDOR_ID'>";
+                    while ($row = $r_set->fetch_assoc()) {
+                        // echo"<option value=$row[VENDOR_ID]>Vendor $row[VENDOR_ID]: $row[v_name]</option>";
+                        echo "<option value=" . $row['VENDOR_ID'] . ">" . $row['v_name'] . "</option>";
+                    }
+                    echo "</select>";
+                }
+
+                ?>
+
+                <br><br>
 
                 <!-- Asking for date purchased -->
                 <label for="prod_date_purchased"> Date Purchased: </label><br>
-                <input type="text" id="prod_date_purchased" name="prod_date_purchased"
-                value="<?php if (isset($_POST['prod_date_purchased'])) echo $_POST['prod_date_purchased'];?>">
+                <input type="text" id="prod_date_purchased" name="prod_date_purchased" value="<?php if (isset($_POST['prod_date_purchased'])) echo $_POST['prod_date_purchased']; ?>">
                 <br>
                 <span> <?php if (isset($purchase_date_error)) echo $purchase_date_error; ?> </span>
                 <br><br>
 
                 <!-- need to add validation to be a # -->
                 <label for="prod_purchase_cost"> Purchase Cost: </label><br>
-                <input type="text" id="prod_purchase_cost" name="prod_purchase_cost"
-                value="<?php if (isset($_POST['prod_purchase_cost'])) echo $_POST['prod_purchase_cost'];?>">
+                <input type="text" id="prod_purchase_cost" name="prod_purchase_cost" value="<?php if (isset($_POST['prod_purchase_cost'])) echo $_POST['prod_purchase_cost']; ?>">
                 <br>
                 <span> <?php if (isset($purchase_cost_error)) echo $purchase_cost_error; ?> </span>
                 <br><br>
