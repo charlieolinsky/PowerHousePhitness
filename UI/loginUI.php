0e5e4c40e402797a477a4b29a5a7eab6507903a2
ericadubie <ericadubie14@gmail.com>
2022-11-03 23:29:24 -0400

    Login Page (not linked to php)

---
+++ UI/loginUI.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+     <title>Login</title>
+
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+     <meta name="description" content="">
+     <meta name="keywords" content="">
+     <meta name="author" content="">
+     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+     <link rel="stylesheet" href="./css/bootstrap.min.css">
+     <link rel="stylesheet" href="./css/font-awesome.min.css">
+     <link rel="stylesheet" href="./css/aos.css">
+
+     <!-- MAIN CSS -->
+     <link rel="stylesheet" href="./css/tooplate-php-style.css">
+<!--
+Tooplate 2119 Gymso Fitness
+https://www.tooplate.com/view/2119-gymso-fitness
+-->
+</head>
+
+<!-- <section class="login" id="login">
+    <p style="background-image:url('../images/login-bg.jpg') ;"
+     hero d-flex flex-column justify-content-center align-items-center  
+    <div class="bg-overlay">
+         <div class="center">
+            <div class="flex-container">
+                <div class="team-info">
+                    <input type="text" class="form-control" name="username" placeholder="Username">
+                    <input type="text" class="form-control" name="passoword" placeholder="Password">
+    
+                </div>
+            </div>
+         </div> 
+    </div>
+ </section>  -->
+
+<body style="background-color:#171819"></body>
+<login-title>P H P</login-title>
+
+    <!-- <div class="flex-container">
+        <div class="modal-body">
+            <input type="text" class="form-control" name="Username" placeholder="Username" required>
+
+            <input type="tel" class="form-control" name="Password" placeholder="Password" required>
+
+            <button type="submit" class="form-control" id="submit-button" name="submit">Login</button>
+        </div>
+    </div> -->
+    <div class = "login-container">
+        <!-- <div class="modal-content">
+            <div class="modal-header"> -->
+                <h2 class="modal-title" id="membershipFormLabel">Login</h2>
+            <!-- </div>
+        </div>  -->
+        <input type="text" class="form-control" name="Username" placeholder="Username" required>
+        <input type="password" class="form-control" name="Password" placeholder="Password" required>
+        <button type="submit" class="form-control" id="submit-button" name="submit">Login</button>
+    </div>
+       
+
+   <!-- SCRIPTS -->
+   <script src="js/jquery.min.js"></script>
+   <script src="js/bootstrap.min.js"></script>
+   <script src="js/aos.js"></script>
+   <script src="js/smoothscroll.js"></script>
+   <script src="js/custom.js"></script>
+</body>
+</html>
+
+
