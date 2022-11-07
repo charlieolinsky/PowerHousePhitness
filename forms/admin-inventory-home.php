f67473108ddd91c15c83651a059e7265453a470f
beesanne <beesanne.kurzum@gmail.com>
2022-11-06 20:33:27 -0500

    created an admin home page for inventory

---
+++ forms/admin-inventory-home.php
@@ -0,0 +1,19 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Inventory Home</title>
+</head>
+
+<body>
+    <div>
+        <a href="admin-inventory-new.php"> <button> Add New Inventory Item</button></a>
+        <a href="admin-inventory-update.php"> <button> Update Current Inventory Item</button></a>
+
+        <!-- <input type="button" name="Add New" value="Add New"> -->
+    </div>
+</body>
+
+</html>
