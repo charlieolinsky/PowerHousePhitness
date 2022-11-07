00bdb2e2128aa58531d54ac40631eae48d70349a
beesanne <beesanne.kurzum@gmail.com>
2022-11-06 20:33:52 -0500

    created a php file to update current inventory

---
+++ forms/admin-inventory-update.php
@@ -0,0 +1,21 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- select current inventory item 
+    update description
+    update image
+    update price 
+    update quantity
+    update purchase cost
+    update date purchased -->
+
+    
+
+    
+</body>
+</html>
