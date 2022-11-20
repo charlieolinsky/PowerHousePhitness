34bb323f0c211fc1e6c83d84eda6599358d0cc2e
beesanne <beesanne.kurzum@gmail.com>
2022-11-20 16:22:58 -0500

    cleaned up files

--- forms/equip-rental-member.php
+++ forms/equip-rental-member.php
@@ -131,7 +131,7 @@
                     <!-- <form action="../classes/testCartClass.php" method="POST"> -->
                     <!-- <form action="../include/add-to-cart2.php" method="POST"> -->
                     <form action="../include/shoppingcartTest.php" method="POST">
-                    <!-- <form action="../forms/test.php" method="POST"> -->
+                    <!-- <form action="../forms/shopping-cart.php" method="POST"> -->
 
 
                             <!-- <form action="../include/ShoppingCartAction.php?action=addToCart&PROD_ID" method="post"> -->
--- forms/temp.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("../sql/connect.php");
-echo("The prof is getting tired!!");
-$xyz = PHP_EOL;
-echo $xyz;
-include_once("../classes/User.php");
-?>
