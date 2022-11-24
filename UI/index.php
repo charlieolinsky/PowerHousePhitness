a12f34dc067aafb95482b0a78cdbcbd01abc4ac6
ericadubie <ericadubie14@gmail.com>
2022-11-23 23:30:30 -0500

    updated comments for index.php

--- UI/index.php
+++ UI/index.php
@@ -22,12 +22,14 @@ Tooplate 2119 Gymso Fitness
 https://www.tooplate.com/view/2119-gymso-fitness
 -->
 </head>
+<!-- Implementing scroll -->
 <body data-spy="scroll" data-target="#navbarNav" data-offset="50">
 
-    <!-- MENU BAR -->
+    <!-- NAV BAR -->
     <nav class="navbar navbar-expand-lg fixed-top">
         <div class="container">
 
+            <!-- PHP Logo -->
             <a class="navbar-brand" href="index.php"><span style="color: var(--primary-color)">P</span>ower <span style="color: var(--primary-color)">H</span>ouse <span style="color: var(--primary-color)">P</span>hitness</a>
 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
@@ -35,16 +37,20 @@ https://www.tooplate.com/view/2119-gymso-fitness
                 <span class="navbar-toggler-icon"></span>
             </button>
 
+            <!-- Create list of links/buttons for different pages -->
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ml-lg-auto">
+                    <!-- Add and link Index page -->
                     <li class="nav-item">
                         <a href="index.php" class="nav-link smoothScroll">Home</a>
                     </li>
 
+                    <!-- Add and link About page -->
                     <li class="nav-item">
                         <a href="about.php" class="nav-link">About Us</a>
                     </li>
 
+                    <!-- Create dropdown menu -->
                     <li class="dropdown">
                         <button class="dropbtn" id="dropdownMenuButton" data-toggle="dropdown"> Services
                             <i class="fa fa-caret-down"></i>
@@ -57,29 +63,26 @@ https://www.tooplate.com/view/2119-gymso-fitness
                         </div> 
                     </li>
 
+                    <!-- Add and link Schedule page -->
                     <li class="nav-item">
                         <a href="schedule.php" class="nav-link">Schedule</a>
                     </li>
 
+                    <!-- Add and link contact section -->
                     <li class="nav-item">
                         <a href="#contact" class="nav-link smoothScroll">Contact</a>
                     </li>
                 </ul>
 
+                <!-- Add User icon -->
                 <ul class="social-icon ml-lg-3">
                         <li><a href="#" class="fa fa-user"></a></li>
                 </ul>
 
+                <!-- Add shopping cart icon with link -->
                 <ul class="social-icon ml-lg-3">
                         <li><a href="../forms/shoppingcart.php" class="fa fa-shopping-cart"></a></li>
                 </ul>
-
-                <!-- <ul class="social-icon ml-lg-3">
-                    <li><a href="https://fb.com/tooplate" class="fa fa-facebook"></a></li>
-                    <li><a href="#" class="fa fa-twitter"></a></li>
-                    <li><a href="#" class="fa fa-instagram"></a></li>
-                </ul> -->
-
             </div>
 
         </div>
@@ -87,12 +90,14 @@ https://www.tooplate.com/view/2119-gymso-fitness
 
 
      <!-- HERO -->
+     <!-- Implements background photo with darker color overlay -->
      <section class="hero d-flex flex-column justify-content-center align-items-center" id="home">
 
             <div class="bg-overlay"></div>
                <div class="container">
                     <div class="row">
 
+                        <!-- Initial text with "get started" and "learn more" buttons-->
                          <div class="col-lg-8 col-md-10 mx-auto col-12">
                               <div class="hero-text mt-5 text-center">
 
@@ -112,10 +117,11 @@ https://www.tooplate.com/view/2119-gymso-fitness
      </section>
 
 
+     <!-- Feature section -->
      <section class="feature" id="feature">
         <div class="container">
             <div class="row">
-
+            <!-- Advertisement info and working hours schedule with fade up capability -->
                 <div class="d-flex flex-column justify-content-center ml-lg-auto mr-lg-5 col-lg-5 col-md-6 col-12">
                     <h2 class="mb-3 text-white" data-aos="fade-up">New to PHP?</h2>
 
@@ -123,8 +129,8 @@ https://www.tooplate.com/view/2119-gymso-fitness
 
                     <p data-aos="fade-up" data-aos-delay="200">This is free HTML template by <a rel="nofollow" href="https://www.tooplate.com" target="_parent">Tooplate</a> for your commercial website.</p>
 
+                    <!-- Button to bring user to membership page -->
                     <a href="services.php#membership" class="btn custom-btn bg-color mt-3" data-aos="fade-up" data-aos-delay="300">Become a member today</a>
-                    <!-- data-toggle="modal" data-target="#membershipForm" -->
                 </div>
 
                 <div class="mr-lg-auto mt-3 col-lg-4 col-md-6 col-12">
@@ -154,16 +160,12 @@ https://www.tooplate.com/view/2119-gymso-fitness
         </div>
     </section>
 
-     <!-- CLASS -->
-     
-     
 
-
-     <!-- CONTACT -->
+     <!-- Contact section-->
      <section class="contact section" id="contact">
           <div class="container">
                <div class="row">
-
+                    <!-- Ask us Anything form (fade up capability) -->
                     <div class="ml-auto col-lg-5 col-md-6 col-12">
                         <h2 class="mb-4 pb-2" data-aos="fade-up" data-aos-delay="200">Feel free to ask anything</h2>
 
@@ -178,16 +180,17 @@ https://www.tooplate.com/view/2119-gymso-fitness
                         </form>
                     </div>
 
+                    <!-- Map Location feature -->
                     <div class="mx-auto mt-4 mt-lg-0 mt-md-0 col-lg-5 col-md-6 col-12">
                         <h2 class="mb-4" data-aos="fade-up" data-aos-delay="600">Where you can <span>find us</span></h2>
 
                         <p data-aos="fade-up" data-aos-delay="800"><i class="fa fa-map-marker mr-1"></i> 1 Hawk Drive, New Paltz, NY 12561</p>
-<!-- How to change your own map point
-	1. Go to Google Maps
-	2. Click on your location point
-	3. Click "Share" and choose "Embed map" tab
-	4. Copy only URL and paste it within the src="" field below
--->
+                            <!-- How to change your own map point
+                                1. Go to Google Maps
+                                2. Click on your location point
+                                3. Click "Share" and choose "Embed map" tab
+                                4. Copy only URL and paste it within the src="" field below
+                            -->
                         <div class="google-map" data-aos="fade-up" data-aos-delay="900">
                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11908.66173540842!2d-74.09329986623278!3d41.73852442931584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89dd227ac44b77b5%3A0x45cf0fa9fe24d389!2s1%20Hawk%20Dr%2C%20New%20Paltz%2C%20NY%2012561!5e0!3m2!1sen!2sus!4v1665685123039!5m2!1sen!2sus" width="1920" height="250" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
                         </div>
@@ -205,6 +208,7 @@ https://www.tooplate.com/view/2119-gymso-fitness
           <div class="container">
                <div class="row">
 
+               <!-- Credits to template owner -->
                     <div class="ml-auto col-lg-4 col-md-5">
                         <p class="copyright-text">Copyright &copy; Power House Phitness <br>
                             (2020 Gymso Fitness Co. Template)
@@ -212,6 +216,7 @@ https://www.tooplate.com/view/2119-gymso-fitness
                         <br>Design: <a href="https://www.tooplate.com">Tooplate</a></p>
                     </div>
 
+                    <!-- (Fake) Contact Information -->
                     <div class="d-flex justify-content-center mx-auto col-lg-5 col-md-7 col-12">
                         <p class="mr-4">
                             <i class="fa fa-envelope-o mr-1"></i>
@@ -226,7 +231,7 @@ https://www.tooplate.com/view/2119-gymso-fitness
      </footer>
 
     <!-- Modal -->
-    <div class="modal fade" id="membershipForm" tabindex="-1" role="dialog" aria-labelledby="membershipFormLabel" aria-hidden="true">
+    <!-- <div class="modal fade" id="membershipForm" tabindex="-1" role="dialog" aria-labelledby="membershipFormLabel" aria-hidden="true">
       <div class="modal-dialog" role="document">
 
         <div class="modal-content">
@@ -262,7 +267,7 @@ https://www.tooplate.com/view/2119-gymso-fitness
           <div class="modal-footer"></div>
 
         </div>
-      </div>
+      </div> -->
     </div>
 
      <!-- SCRIPTS -->
