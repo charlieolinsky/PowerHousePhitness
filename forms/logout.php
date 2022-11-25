7166331448d381367db6f9748c621d2a0c87945e
charlieolinsky <charlesolinsky@gmail.com>
2022-11-25 14:54:16 -0500

    logout edit

--- forms/logout.php
+++ forms/logout.php
@@ -1,10 +1,14 @@
+
 <?php
     include_once("../include/global_inc.php");
     
-    //NOTE: May need to store cart data to DB on logout
-    
     //Destroys Session
-    Session::destroy(); 
+    $s = new Session();
+    $s->destroy(); 
+
+    //Send user back to login
+    header("Location: login.php");
+    die(); 
 
 
 ?>
