f0ca7dc9bd9a0f27efb66d941fdb37903f2b2542
ericadubie <ericadubie14@gmail.com>
2022-11-24 19:09:39 -0500

    applied partial style to account_tab and made account_tab_COPY.php

--- forms/equip-rental-employee.php
+++ forms/equip-rental-employee.php
@@ -144,6 +144,13 @@
 
     </div>
 
+     <!-- SCRIPTS -->
+     <script src="js/jquery.min.js"></script>
+     <script src="js/bootstrap.min.js"></script>
+     <script src="js/aos.js"></script>
+     <script src="js/smoothscroll.js"></script>
+     <script src="js/custom.js"></script>
+
 </body>
 
 </html>
