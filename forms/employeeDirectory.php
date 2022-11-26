519389180f32579bdd931d2e798ef2b6bd08578f
charlieolinsky <charlesolinsky@gmail.com>
2022-11-25 22:31:13 -0500

    Directory Hierarchy Added

---
+++ forms/employeeDirectory.php
@@ -0,0 +1,27 @@
+<?php
+    include_once("../include/global_inc.php");
+    Roles::minAccess(3, "../UI/index.php");
+    $s = new Session();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EMPLOYEE</title>
+</head>
+<body>
+    <div>
+        <h3>Employee Privileges: </h3>
+        <a href="equip-rental-employee.php">Manage Equipment Rental</a><br>
+
+        <br><br><!--Hi Erica! I know how much you love these break statements xoxo -charlie -->
+        <a href="../UI/index.php">Return Home</a>
+    </div> 
+
+    
+    
+</body>
+</html>
--- forms/financialDirectory.php
+++ forms/financialDirectory.php
@@ -1,9 +1,7 @@
 <?php
     include_once("../include/global_inc.php");
-    $s = new Session(); 
-    
-    //Only admins can view the admin directory 
-    Roles::minAccess(5, "../forms/financialDirectory.php"); 
+    Roles::minAccess(4, "employeeDirectory.php");
+    $s = new Session();
 ?>
 
 <!DOCTYPE html>
@@ -12,16 +10,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ADMIN</title>
+    <title>FINANCE</title>
 </head>
 <body>
     <div>
-        <h3>Admin Privileges: </h3>
-        <a href="admin_search_users.php">Search for a User</a><br>
-        <a href="admin-inventory-home.php">View Inventory</a>
-
-        <br> <!--Hi Erica! I know how much you love these break statements xoxo -charlie -->
-
         <h3>Employee Privileges: </h3>
         <a href="equip-rental-employee.php">Manage Equipment Rental</a>
 
