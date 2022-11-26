7d1e0b1efce5a6e55ebf20244d3b40e28aa25843
beesanne <beesanne.kurzum@gmail.com>
2022-11-26 15:03:52 -0500

    added code to decrease the quantity in stock when item is checked out.

--- forms/checkout.php
+++ forms/checkout.php
@@ -8,127 +8,139 @@ require_once("../classes/ShoppingCart.php");
 <!DOCTYPE html>
 
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Checkout</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
 
-        <link rel="stylesheet" href="../css/checkout.css">
-    </head>
-    
-    <body>
-        <h1>Checkout</h1>
-        <div>
-            <h3>Billing Address</h3>
-            <form name ="placeOrder" action="placeorder.php" onsubmit="submit()"> 
-                    
-                <label for="fname"> First Name </label>
-                <input type="text" name="fname" placeholder="First Name" required><br><br>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Checkout</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="">
 
-                <label for="lname"> Last Name </label>
-                <input type="text" name="lname" placeholder="Last Name" required><br><br>
+    <link rel="stylesheet" href="../css/checkout.css">
+</head>
 
-                <label for="email"> Email </label>
-                <input type="text" name="email" placeholder="you@example.com"><br><br>
-
-                <label for="adr"> Address </label>
-                <input type="text" name="adr" placeholder="Address"><br><br>
-
-                <label for="city"> City </label>
-                <input type="text" name="city" placeholder="New Paltz"><br><br>
-
-                <label for="state"> State </label>
-                <input type="text" name="state" placeholder="New York">
-
-                <label for="zip"> Zip </label> 
-                <input type="text" name="zip" placeholder="12561"><br><br>
-                
-                
-                <h3> Payment Information </h3>
-                <label for="cname">Name on Card</label>
-                <input type="text" name="cname" placeholder="John M Doe"><br><br>
-
-                <label for="cnum">Card number </label>
-                <input type="text" name="cnum" placeholder="1111-2222-3333-4444"><br><br>
-                
-                <label for="expdate">Exp Date</label>
-                <input type="text" name="expdate" placeholder="MM/YY"><br><br>
-                
-                <label for="cvv">CVV</label>
-                <input type="text" name="cvv" placeholder="123"><br><br>
-
-                <input type="submit" value="Place Order" >
-                    
-            </form>                
-               
-        </div>
-
-        <div>
-            <h4> Cart </h4>
-            <br></br>
-            <table>
-                <tr>
-                    <th>Image</th>
-                    <th>Item</th>
-                    <th>Price</th>
-                    <th>Quantity</th>
-                    <th>Subtotal</th>
-                </tr>
-
-                <?php
-
-                $grand_total = 0;
-
-                //go through each item in the cart and display its information from the prod data table
-                foreach ($_SESSION['cart'] as $key => $val) {
-
-                    $query = "SELECT * FROM `prod-data` WHERE PROD_ID=$key;";
-                    $result = $dbconn->query($query);
-                    while ($row = $result->fetch_assoc()) {
+<body>
+    <h1>Checkout</h1>
+    <div>
+        <h3>Billing Address</h3>
+        <!-- <form name="placeOrder" action="placeorder.php" onsubmit="submit()"> -->
+        <form action="../forms/placeorder.php" method="POST">
 
 
-                        $sub = $val * $row['prod_price'];
-                        $grand_total += $sub;
+            <label for="fname"> First Name </label>
+            <input type="text" name="fname" placeholder="First Name" required><br><br>
 
-                ?>
+            <label for="lname"> Last Name </label>
+            <input type="text" name="lname" placeholder="Last Name"><br><br>
 
-                        <tr>
-                            <td> <?php echo "<img src=$row[prod_image]>" ?></td>
-                            <td> <?php echo $row['prod_name'] ?></td>
-                            <td> <?php echo "$" . $row['prod_price'] ?></td>
-                            <td> <?php echo $val ?> </td>
-                            <td> <?php echo "$" . number_format($sub, 2) ?></td>
-                        </tr>
-                <?php
-                    }
+            <label for="email"> Email </label>
+            <input type="text" name="email" placeholder="you@example.com"><br><br>
+
+            <label for="adr"> Address </label>
+            <input type="text" name="adr" placeholder="Address"><br><br>
+
+            <label for="city"> City </label>
+            <input type="text" name="city" placeholder="New Paltz"><br><br>
+
+            <label for="state"> State </label>
+            <input type="text" name="state" placeholder="New York">
+
+            <label for="zip"> Zip </label>
+            <input type="text" name="zip" placeholder="12561"><br><br>
+
+
+            <h3> Payment Information </h3>
+            <label for="cname">Name on Card</label>
+            <input type="text" name="cname" placeholder="John M Doe"><br><br>
+
+            <label for="cnum">Card number </label>
+            <input type="text" name="cnum" placeholder="1111-2222-3333-4444"><br><br>
+
+            <label for="expdate">Exp Date</label>
+            <input type="text" name="expdate" placeholder="MM/YY"><br><br>
+
+            <label for="cvv">CVV</label>
+            <input type="text" name="cvv" placeholder="123"><br><br>
+
+            <input type="submit" value="Place Order" name="placeorder">
+
+            <!-- <input type="submit" value="Place Order" name="checkout"> -->
+
+
+            <!-- </form> -->
+
+    </div>
+
+    <div>
+        <h4> Cart </h4>
+        <br></br>
+        <table>
+            <tr>
+                <th>Image</th>
+                <th>Item</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Subtotal</th>
+            </tr>
+
+            <?php
+
+            $grand_total = 0;
+
+            //go through each item in the cart and display its information from the prod data table
+            foreach ($_SESSION['cart'] as $key => $val) {
+
+                $query = "SELECT * FROM `prod-data` WHERE PROD_ID=$key;";
+                $result = $dbconn->query($query);
+                while ($row = $result->fetch_assoc()) {
+
+
+                    $sub = $val * $row['prod_price'];
+                    $grand_total += $sub;
+
+            ?>
+
+                    <tr>
+                        <td> <?php echo "<img src=$row[prod_image]>" ?></td>
+                        <td> <?php echo $row['prod_name'] ?></td>
+                        <td> <?php echo "$" . $row['prod_price'] ?></td>
+                        <td> <?php echo $val ?> </td>
+                        <td> <?php echo "$" . number_format($sub, 2) ?></td>
+                    </tr>
+
+
+            <?php
+
                 }
 
-                if (empty($_SESSION['cart'])) {
-                    echo "<tr><td colspan='4'> Your Cart is Empty </td></tr";
-                } else {
-                    echo "<tr><td colspan='4'> Total Price: $" . number_format($grand_total, 2) . "</td></tr";
-                }
-                ?>      
-            </table>
-
-        
-        </div>
-    </body>
-
-    <script>
-        function submit()
-        {
-            if ($.trim($("fname").val()) === "" || $.trim($("lname").val()) === "")
-            // var empt = document.forms['firstname'].value;
-            // if (empt == "")
-            {
-                alert ('Please fill out this field.');
-                return false;
+                
             }
+
+            if (empty($_SESSION['cart'])) {
+                echo "<tr><td colspan='4'> Your Cart is Empty </td></tr";
+            } else {
+                echo "<tr><td colspan='4'> Total Price: $" . number_format($grand_total, 2) . "</td></tr";
+            }
+            ?>
+
+
+        </table>
+        </form>
+
+    </div>
+</body>
+
+<script>
+    function submit() {
+        if ($.trim($("fname").val()) === "" || $.trim($("lname").val()) === "")
+        // var empt = document.forms['firstname'].value;
+        // if (empt == "")
+        {
+            alert('Please fill out this field.');
+            return false;
         }
-    </script>
-    
+    }
+</script>
+
 </html>
--- forms/placeorder.php
+++ forms/placeorder.php
@@ -1,20 +1,55 @@
+<?php
+require_once("../classes/ShoppingCart.php");
+
+//go through each item in the cart and grab its original quantity from the prod data table
+foreach ($_SESSION['cart'] as $key => $val) {
+    //get the quanitity for that product 
+    $query = "SELECT * FROM `prod-data` WHERE PROD_ID=$key;";
+    $result = $dbconn->query($query);
+    //for each prod in the array
+    while ($row = $result->fetch_assoc()) {
+
+        //if the place order button is clicked 
+        if (isset(($_POST['placeorder']))) {
+            $instock = $row['prod_quantity'];
+            //update the prod quantity in the prod-data table 
+            $updateInStock = "UPDATE `prod-data` SET prod_quantity=$instock-$val WHERE PROD_ID=$key";
+            if ($dbconn->query($updateInStock) === TRUE) {
+                // echo "quantity decreased";
+            }
+                unset($_SESSION['order_id']);
+                $_SESSION['cart'] = array();
+        
+        }
+    }
+
+}
+
+?>
+
+
+
+
+
+
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
-    
+
 </head>
+
 <body>
     <div class="placeorder content-wrapper">
         <h1>Your Order Has Been Placed</h1>
         <p>Thank you for ordering with us, we'll contact you by email with your order details.</p>
+        <a href="../forms/equip-rental-member.php"> Return to Shopping </a>
+
     </div>
 </body>
+
 </html>
-
-
-
-
