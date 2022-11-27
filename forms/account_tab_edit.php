b74ae6482ab8ef46b926f740ee8c4e3c84c12caf
ericadubie <ericadubie14@gmail.com>
2022-11-27 00:29:13 -0500

    account_tab_edit.php styled

--- forms/account_tab_edit.php
+++ forms/account_tab_edit.php
@@ -71,66 +71,127 @@ if (isset($_POST['remove_user']))
 
 ?>
 
-<html>
-  <head>
-    <title>My Account Tab</title>
-  </head>
-<body>
-<main> 
-<h1>My Account</h1>
-<div class = "account-info">
-    <dl>
-        <dt>First Name: 
-        <?php
-          echo $s->read('fname');
-        ?>
-           <!-- The form -->
-      <div class="form-popup" id="myForm">
-      <form action="../forms/account_tab_edit.php" method="POST">   
+<html lang="en">
 
-            <label for="newName"><b>Edit First Name</b></label>
-            <input type="name" placeholder="Enter new first name" name="newName" required>
-            <input type="submit" name = "fn" value = "Submit">
-            <!-- <button type="submit" class="btn">Submit</button> -->
-          </form>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Account</title>
+
+    <link rel="stylesheet" href="../UI/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../UI/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../UI/css/aos.css">
+    <link rel="stylesheet" href="../UI/css/tooplate-php-style.css">
+
+</head>
+  <body style="background-color: var(--dark-color)">
+  <main> 
+    <!-- Title -->
+    <div class="equip-title">
+          <h1 style="color: var(--primary-color)">My Account</h1>
+    </div>
+
+    <!-- Edit Information Form -->
+    <div class = "inventory-container">
+        <div class="column">
+          <!-- First Name -->
+          <p style="color: var(--primary-color)">
+            First Name: 
+            <?php
+              echo $s->read('fname');
+            ?>
+          </p>
+
+          <!-- Edit First Name -->
+          <div class="row" style="justify-content: center">
+                  <!-- The form -->
+              <div class="form-popup" id="myForm">
+              <form action="../forms/account_tab_edit.php" method="POST">   
+
+              <label for="newName"><b>Edit First Name:</b></label>
+              <input type="name" placeholder="Enter new first name" name="newName" required>
+              </form>
+              </div>
+          </div>
+          <input type="submit" class="btn edit-btn" name="fn" value="Submit">
+
+          <br><br>
+
+          <!-- Last Name -->
+          <p style="color: var(--primary-color)">
+            Last Name: 
+            <?php
+              echo $s->read('lname');
+            ?>
+          </p>
+
+          <!-- Edit Last Name -->
+          <div class="row" style="justify-content: center">
+              <!-- The form -->
+              <div class="form-popup" id="myForm">
+              <form action="../forms/account_tab_edit.php" method="POST">  
+  
+                <label for="newLname"><b>Edit Last Name:</b></label>
+                <input type="name" placeholder="Enter new last name" name="newLname" required>
+              </form>
+              </div>
+          </div>
+          <input type="submit" class="btn edit-btn" name="ln" value="Submit">
+
+          <br><br>
+
+          <!-- Change Password Title -->
+          <p style="color: var(--primary-color)">
+              Change Password
+          </p>
+
+          <!-- Change Password -->
+          <div class="row" style="justify-content: center">
+            <div class="form-popup" id="myForm">
+            <form action="../forms/account_tab_edit.php" method="POST">  
+
+              <label for="newAddress"><b>Password:</b></label>
+              <input type="password" placeholder="Enter new password" name="newPassword" required>
+              <input type="password" placeholder="Confirm password" name="vPass" required>
+
+           </form>
+          </div>
+        </div>
+        <input type="submit" class="btn edit-btn" name="pass" value="Submit">
+
       </div>
+
+      <br> 
+
+      <div class="column">
+        <div class="row" style="justify-content: space-around">
+          <a href="../UI/services.php#membership">Upgrade Your Membership</a>
+          <a href="cancelMembership.php">Cancel Your Membership</a>
+        </div>
+        <br>
+          <form action="../forms/account_tab_edit.php" method="POST">  
+          <input type="submit" class="btn cart-btn" name="remove_user" value="Delete Account">
+      </div>
+
+
+    </div>
+
+    <!-- <dl>
+        
         </dt>
 
         <dt>
-        <br>    
-        Last Name: 
-        <?php
-          echo $s->read('lname');
-        ?>
-           <!-- The form -->
-      <div class="form-popup" id="myForm">
-      <form action="../forms/account_tab_edit.php" method="POST">  
-  
-            <label for="newLname"><b>Edit Last Name</b></label>
-            <input type="name" placeholder="Enter new last name" name="newLname" required>
-            <input type="submit" name = "ln" value = "Submit">
-          </form>
-      </div>
-        <br>    
-           <!-- The form -->
-      <div class="form-popup" id="myForm">
-      <form action="../forms/account_tab_edit.php" method="POST">  
-            <h4>Change Password</h4>
-
-            <label for="newAddress"><b>Password</b></label>
-            <input type="password" placeholder="Enter new password" name="newPassword" required>
-            <input type="password" placeholder="Confirm password" name="vPass" required>
-
-            <input type="submit" name = "pass" value = "Submit">
-          </form>
-      </div>
+        <br>    --> 
+        
+     
       <br>
       <!-- <button class="btn delete account">Delete Account</button> -->
-      <form action="../forms/account_tab_edit.php" method="POST">  
-      <input type="submit" name = "remove_user" value = "Delete Account">
-      </dl>
-      <a href="../UI/services.php#membership">Upgrade Your Membership</a>
-      <a href="cancelMembership.php">Cancel Your Membership</a>
+      <!-- <form action="../forms/account_tab_edit.php" method="POST">  
+      <input type="submit" name = "remove_user" value = "Delete Account"> 
+      </dl> -->
+      <!-- <a href="../UI/services.php#membership">Upgrade Your Membership</a>
+      <a href="cancelMembership.php">Cancel Your Membership</a> -->
 </div>
 </main>
 </body>
