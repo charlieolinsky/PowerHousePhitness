9c31a7f24bd2d77cf31b47e42fa9a06387aabc33
beesanne <beesanne.kurzum@gmail.com>
2022-12-01 06:53:10 -0500

    atempt to fix issue on pc

--- include/add-products.php
+++ include/add-products.php
@@ -3,16 +3,20 @@
 
 include_once("../sql/connect.php");
 
-$prodname = $_POST['prod_name'];
-$desc = $_POST['prod_desc'];
-$img = $_FILES['prod_image']['name'];
-$price =  $_POST['prod_price'] ;
-$quant = $_POST['prod_quantity'] ;
-$vendor = $_POST['VENDOR_ID'];
-$purchdate = $_POST['prod_date_purchased'];
-$purchcost = $_POST['prod_purchase_cost'] ;
+if(isset($_POST['prod_name']) and isset($_POST['prod_desc']) and isset($_POST['prod_name']) and  isset($_FILES['prod_image']['name'])  and isset($_POST['prod_price'])  
+and isset($_POST['prod_quantity'])  and isset($_POST['VENDOR_ID']) and isset($_POST['prod_date_purchased']) and isset($_POST['prod_purchase_cost']) ){
+
+  $prodname = $_POST['prod_name'];
+  $desc = $_POST['prod_desc'];
+  $img = $_FILES['prod_image']['name'];
+  $price =  $_POST['prod_price'] ;
+  $quant = $_POST['prod_quantity'] ;
+  $vendor = $_POST['VENDOR_ID'];
+  $purchdate = $_POST['prod_date_purchased'];
+  $purchcost = $_POST['prod_purchase_cost'] ;
 
 
+}          
 
 $sql = "INSERT INTO `prod-data` 
 (`prod_name`, 
