34e9e4a82b08917ad1092e5fc587de091f45ccab
ericadubie <ericadubie14@gmail.com>
2022-12-01 02:46:49 -0500

    Styled financialDirectory.php

--- forms/financialDirectory.php
+++ forms/financialDirectory.php
@@ -5,25 +5,99 @@
 ?>
 
 <!DOCTYPE html>
+
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FINANCE</title>
+    <title>Finance Portal</title>
+
+    <link rel="stylesheet" href="../UI/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../UI/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../UI/css/aos.css">
+    <link rel="stylesheet" href="../UI/css/tooplate-php-style.css">
+
 </head>
-<body>
-    <div>
-        <h1>Finance Portal</h1>
-        
-        <br>
-        
-        <a href="../classes/FinancialReports.php">Reports</a>
 
-        <br><br><!--Hi Erica! I know how much you love these break statements xoxo -charlie -->
+<body style="background-color: var(--dark-color)">
+    <!-- NAV BAR -->
+    <nav class="navbar navbar-expand-lg fixed-top">
+        <div class="container">
 
-        <a href="../UI/index.php">Return Home</a>
+            <a class="navbar-brand" href="adminDirectory.php"><span style="color: var(--primary-color)">P</span>ower <span style="color: var(--primary-color)">H</span>ouse <span style="color: var(--primary-color)">P</span>hitness</a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- Create list of links/buttons for different pages -->
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-lg-auto">
+                    <!-- Add and link Index page -->
+                    <li class="nav-item">
+                        <a href="index.php" class="nav-link smoothScroll">Home</a>
+                    </li>
+
+                    <!-- Add and link About page -->
+                    <li class="nav-item">
+                        <a href="about.php" class="nav-link">About Us</a>
+                    </li>
+
+                    <!-- Create dropdown menu -->
+                    <li class="dropdown">
+                        <button class="dropbtn" id="dropdownMenuButton" data-toggle="dropdown"> Services
+                            <i class="fa fa-caret-down"></i>
+                        </button>
+
+                        <div class="dropdown-content">
+                            <a href="services.php#classes">Classes </a>
+                            <a href="services.php#membership">Memberships </a>
+                            <a href="..\forms\equip-rental-member.php">Equipment </a>
+                        </div>
+                    </li>
+
+                    <!-- Add and link Schedule page -->
+                    <li class="nav-item">
+                        <a href="schedule.php" class="nav-link">Schedule</a>
+                    </li>
+
+                    <!-- Add and link contact section -->
+                    <li class="nav-item">
+                        <a href="index.php#contact" class="nav-link smoothScroll">Contact</a>
+                    </li>
+                </ul>
+
+                <!-- Add User icon -->
+                <ul class="social-icon ml-lg-3">
+                    <li><a href="../forms/account_tab.php" class="fa fa-user"></a></li>
+                </ul>
+
+                <!-- Add shopping cart icon with link -->
+                <ul class="social-icon ml-lg-3">
+                    <li><a href="../forms/shoppingcart.php" class="fa fa-shopping-cart"></a></li>
+                </ul>
+            </div>
+
+        </div>
+    </nav>
+
+    <br><br><br>
+    <main style="text-align: center">
+        <div class="admin-title">
+            <h1 style="color: var(--primary-color)">Finance Portal</h1>
+        </div>
+
+        <div class="inventory-container" style="margin-top: 100px">
+            <div class="column">
+                <h3>Finance Personnel</h3>
+                <a href="../classes/FinancialReports.php" class="btn edit-btn bg-color" style="margin-top: 10px; min-width: -webkit-fill-available">Reports</a>
+                <br>
+                <a href="../UI/index.php" class="btn class-btn bordered mt-3" style="margin-top: 5px; min-width: -webkit-fill-available">Return Home</a>
+            </div>
+        </div>
     </div> 
-    
+    </main>
 </body>
 </html>
