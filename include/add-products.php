2c221a50cf16c41d9110aa364c22d6e1aa90a45a
beesanne <beesanne.kurzum@gmail.com>
2022-11-30 21:35:02 -0500

    removed old query

--- include/add-products.php
+++ include/add-products.php
@@ -3,30 +3,6 @@
 
 include_once("../sql/connect.php");
 
-// $sql = "INSERT INTO `prod-data` 
-//             (`prod_name`, 
-//             `prod_desc`,
-//             `prod_image`, 
-//             `prod_price`, 
-//             `prod_quantity`, 
-//             `VENDOR_ID`, 
-//             `prod_date_purchased`,
-//             `prod_purchase_cost`)
-// VALUES 
-//         ( '".$_POST['prod_name']."', 
-//         '".$_POST['prod_desc']."',
-//         '../UI/images/prod_images/".$_FILES['prod_image']['name']."',
-//         '".$_POST['prod_price']."', 
-//         '".$_POST['prod_quantity']."',
-//         '".$_POST['VENDOR_ID']."',
-//         '".$_POST['prod_date_purchased']."',
-//         '".$_POST['prod_purchase_cost']."'
-//         )";
-
-
-
-
-
 $prodname = $_POST['prod_name'];
 $desc = $_POST['prod_desc'];
 $img = $_FILES['prod_image']['name'];
