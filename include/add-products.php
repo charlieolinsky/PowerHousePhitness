d3e6d553eb96cca27b7fdecded357ab7c1cf14c8
beesanne <beesanne.kurzum@gmail.com>
2022-12-05 15:20:19 -0500

    added validation variable to set to true / false

--- include/add-products.php
+++ include/add-products.php
@@ -1,7 +1,7 @@
 
 <?php
 
-//if these fields are set we can contiune -- needed to avoid errors on PC
+//if these fields are set we can contiune
 if (
   isset($_POST['prod_name']) and isset($_POST['prod_desc']) and isset($_POST['prod_name']) and  isset($_FILES['prod_image']['name'])  and isset($_POST['prod_price'])
   and isset($_POST['prod_quantity'])  and isset($_POST['VENDOR_ID']) and isset($_POST['prod_date_purchased']) and isset($_POST['prod_purchase_cost'])
@@ -11,17 +11,16 @@ if (
 
   //  QUERY TO INSERT ITEM 
   $sql = $dbconn->prepare("INSERT INTO `prod_data` 
-(`prod_name`, `prod_desc`, `prod_image`, `prod_price`, `prod_quantity`, `VENDOR_ID`, `prod_date_purchased`, `prod_purchase_cost`)
-VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+          (`prod_name`, `prod_desc`, `prod_image`, `prod_price`, `prod_quantity`, 
+          `VENDOR_ID`, `prod_date_purchased`, `prod_purchase_cost`)
+          VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
 
 
   $sql->bind_param("sssiiisi", $prodname, $desc, $img, $price, $quant, $vendor, $purchdate, $purchcost);
 
   //if the data validation is good begin executing the query
-  if (
-    !isset($name_error) and !isset($desc_error) and !isset($price_error) and !isset($quantity_error)
-    and !isset($purchase_date_error) and !isset($purchase_cost_error) and !isset($vendor_error)
-  ) {
+    if($isvalid == TRUE){
+
     // SETTING THE VARIABLES
     $prodname = $_POST['prod_name'];
     $desc = $_POST['prod_desc'];
@@ -34,6 +33,7 @@ VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
 
     // EXECUTING THE QUERY!
     $sql->execute();
+    //display success 
     echo "<p style='color:red; text-align:center'> New inventory item added successfully  <p>";
   }
 
